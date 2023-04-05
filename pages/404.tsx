4b726b46f711f8d774faf5d2f27c7c23bc99bf50
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 00:22:30 +0100

    Initial Commit

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+  return (
+    <div>
+      <h1 className="text-2xl text-blue-500 text-center mt-10">
+        Sorry, Page not found!
+      </h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
