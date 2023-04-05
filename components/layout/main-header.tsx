163c46a1b3cee6e2edf749aba0e4ef3193a674c3
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 00:17:01 +0100

    Create MainHeader component

---
+++ components/layout/main-header.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../ui/Button";
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/solid";
+import Harmburger from "./harmburger";
+import { AddUser, Logo, logo, Search } from "@/assets";
+
+//import AnchorLink from "react-anchor-link-smooth-scroll";
+
+function MainHeader() {
+  return (
+    <header className="flex items-center justify-between mr-20 ">
+      <Image src={logo} alt="LogoImage" width={200} height={200} />
+
+      {/*Navigation Links  */}
+
+      <nav>
+        <ul className="flex flex-row gap-8 text-xl font-urbanist font-medium">
+          <li className="text-colBlue01">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="text-colGray02">
+            <Link href="/howitworks">How It Works</Link>
+          </li>
+          <li className="text-colGray02">
+            <Link href="/featured">Featured</Link>
+          </li>
+          <li className="text-colGray02">
+            <Link href="/testimony">Testimony</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* ICONS */}
+
+      <div className="gap-6 flex flex-row text-colBlue01 ">
+        <Image src={Search} alt="searchIcon" width={30} height={30}></Image>
+
+        <Image src={AddUser} alt="addUserIcon" width={30} height={30}></Image>
+      </div>
+    </header>
+  );
+}
+
+export default MainHeader;
