b38a498079589a16c54a2e4287265090291a96b4
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 00:14:42 +0100

    Initial Commit

---
+++ assets/index.tsx
@@ -0,0 +1,25 @@
+import threePhones from "./threePhones.png";
+import heroImage from "./heroImage.png";
+import laptopImage from "./laptopImage.png";
+import logo from "./logo.png";
+import icon01 from "./icon01.png";
+import icon02 from "./icon02.png";
+import icon03 from "./icon03.png";
+import bodyImage from "./bodyImage.png";
+import AddUser from "./AddUser.svg";
+import Search from "./Search.svg";
+import Logo from "./Logo.svg";
+
+export {
+  threePhones,
+  heroImage,
+  laptopImage,
+  logo,
+  icon01,
+  icon02,
+  icon03,
+  bodyImage,
+  AddUser,
+  Search,
+  Logo,
+};
