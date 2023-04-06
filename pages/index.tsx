8d90a5ced353ff55c7fe870cef85dd6ce907a0dc
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 02:09:42 +0100

    Create the Desktop version of the Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { heroImage } from "@/assets";
 import Head from "next/head";
+import Image from "next/image";
+Image;
 
 export default function Home() {
   return (
@@ -9,6 +12,39 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Logo.svg" />
       </Head>
+
+      {/* Main Container */}
+      <div className="flex flex-col md:flex-row  mx-12  justify-between mt-10">
+        {/* Right Container... Contents and Buttons */}
+
+        <div className="flex flex-col  w-3/5">
+          <h1 className="text-[70px] font-urbanist text-colBlue01 font-semibold leading-1">
+            Find your best home with virtual tour
+          </h1>
+
+          <p className="mt-6 text-colGray01 font-urbanist font-normal text-lg  ">
+            This application for the needs of buying, rent, and selling <br />
+            property. Can see the details of the building you are <br /> looking
+            for using the 3D virtual room feature.
+          </p>
+
+          {/* Buttons Container */}
+
+          <div className="flex flex-col md:flex-row gap-4 mt-10 ">
+            <button className="px-12 py-3 border-2 rounded-lg bg-colBlue04 border-colBlue04 text-white font-urbanist font-semibold ">
+              Coming soon <br /> ios & Playstore
+            </button>
+
+            <button className="px-12 py-3 border-2 rounded-lg bg-white border-colBlue04 text-colBlue01 font-urbanist font-semibold ">
+              Get Early Access
+            </button>
+          </div>
+        </div>
+
+        {/* Left Container... Hero Image */}
+
+        <Image src={heroImage} alt="heroImage" width={600} height={600} />
+      </div>
     </>
   );
 }
