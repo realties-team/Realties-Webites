9e1d9c4cd4a4756cd05924a716387794a2860e46
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 22:45:01 +0100

    Initial Commit

---
+++ components/howitworks/index.tsx
@@ -0,0 +1,59 @@
+import { bodyImage, icon01, icon02, icon03 } from "@/assets";
+import React from "react";
+import Services from "./Services";
+import { ServiceType } from "../ui/type";
+import StepsToUse from "./StepsToUse";
+
+const services: Array<ServiceType> = [
+  {
+    id: 1,
+    icon: icon01,
+    title: "For Real Estate Managers",
+    description: "We are available at all times for discussions ",
+  },
+
+  {
+    id: 2,
+    icon: icon02,
+    title: "Private Agents",
+    description: "We are available at all times for discussions ",
+  },
+
+  {
+    id: 3,
+    icon: icon03,
+    title: "End Users",
+    description: "We are available at all times for discussions ",
+  },
+];
+
+type Props = {};
+
+const HowItWorks = (props: Props) => {
+  return (
+    <>
+      <div>
+        <div className="flex flex-col mt-32 mx-20 ">
+          <h2 className=" flex justify-center items-center text-colBlack01 font-urbanist font-semibold text-2xl ">
+            We Are Providing Best Services
+          </h2>
+
+          <div className="flex flex-row justify-around mt-10 mb-20">
+            {services.map((service: ServiceType) => (
+              <Services
+                key={service.id}
+                icon={service.icon}
+                title={service.title}
+                description={service.description}
+              />
+            ))}
+          </div>
+        </div>
+
+        <StepsToUse />
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
