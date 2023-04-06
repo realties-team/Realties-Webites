7595238e56e3596a69eb9d1bf2f1fb3e7147625b
MarsIfeanyi <marschalice@gmail.com>
2023-04-06 22:45:33 +0100

    Initial Commit

---
+++ components/howitworks/Services.tsx
@@ -0,0 +1,42 @@
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+type Props = {
+  icon: StaticImageData;
+  title: string;
+  description: string;
+};
+
+const Services = ({ icon, title, description }: Props) => {
+  return (
+    <>
+      {/* <div className="flex flex-col mt-14 gap-0 "> */}
+      {/* <h2 className="flex justify-center items-center ">
+          We Are Providing Best Services
+        </h2> */}
+
+      <div className="flex flex-row justify-around mt-10 mb-10">
+        <div className="flex flex-col">
+          {/* <div className="flex justify-center items-center h-screen"></div> */}
+          <Image
+            src={icon}
+            alt="iconsImage"
+            width={50}
+            height={50}
+            className=" flex items-center justify-center mx-auto "
+          />
+          <h3 className="text-center mt-4 text-colBlack01 font-dmsans  font-semibold">
+            {title}{" "}
+          </h3>
+          <p className="mt-2 text-colGray01 font-dmsans font-normal">
+            {description}{" "}
+          </p>
+        </div>
+      </div>
+
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Services;
