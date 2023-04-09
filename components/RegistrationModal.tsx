354ba237b9ba8c87d38b69c509c5a0bdae06165e
MarsIfeanyi <marschalice@gmail.com>
2023-04-09 21:29:24 +0100

    Update component

--- components/RegistrationModal.tsx
+++ components/RegistrationModal.tsx
@@ -16,7 +16,7 @@ const RegistrationModal = ({ isVisible, onClose, children }: Props) => {
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-none flex justify-center items-center"
       id="wrapper"
       onClick={handleClose}
     >
