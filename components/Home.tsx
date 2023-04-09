28291d3fae4f5b7ede5edfce21b377b2e10eb5d0
MarsIfeanyi <marschalice@gmail.com>
2023-04-09 21:29:00 +0100

    Update component

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,4 +1,10 @@
-import { heroImage, rocket } from "@/assets";
+import {
+  AppStoreIcon,
+  heroImage,
+  heroImage2,
+  PlayStoreIcon,
+  rocket,
+} from "@/assets";
 import Image from "next/image";
 import React, { useState } from "react";
 import EarlyAccessForm from "./EarlyAccessForm";
@@ -12,23 +18,22 @@ const Home = (props: Props) => {
   return (
     <>
       {/* Main Container */}
-      <div className="flex flex-col md:flex-row  mx-12  justify-between mt-10">
+      <div className="flex flex-col md:flex-row  mx-12  justify-between mt-20">
         {/* Right Container... Contents and Buttons */}
 
         <div className="flex flex-col  w-3/5">
-          <h1 className="text-[70px] font-urbanist text-colBlue01 font-semibold leading-1">
-            Find your best home with virtual tour
+          <h1 className="text-[70px] font-urbanist text-colBlue01 font-semibold leading-tight">
+            Uniting all your real estate dreams in a single button
           </h1>
 
           <p className="mt-6 text-colGray01 font-urbanist font-normal text-lg  ">
             This application for the needs of buying, rent, and selling <br />
-            property. Can see the details of the building you are <br /> looking
-            for using the 3D virtual room feature.
+            property. Uniting all your real estate dreams in a single button
           </p>
 
           {/* Buttons Container */}
 
-          <div className="flex flex-col md:flex-row gap-4 mt-10 ">
+          {/* <div className="flex flex-col md:flex-row gap-4 mt-10 ">
             <button
               className="px-12 py-3 border-2 rounded-lg bg-colBlue04 border-colBlue04 text-white font-urbanist font-semibold"
               onClick={() => setShowModal(true)}
@@ -42,12 +47,45 @@ const Home = (props: Props) => {
             >
               Get Early Access
             </button>
+          </div> */}
+
+          <div className="flex flex-row gap-4 mt-8 ">
+            <button className="flex flex-row h-16 gap-4 bg-white py-2 px-6 border-2 border-colBlue04 rounded-lg">
+              <Image src={PlayStoreIcon} alt="GooglePlayStoreIcon" width={40} />
+
+              <div className="flex flex-col leading-tight">
+                <p className="text-colBlue02 font-dmsans font-normal">
+                  Coming Soon{" "}
+                </p>
+                <h2 className="font-urbanist font-bold text-colBlue02 text-lg">
+                  Google Play
+                </h2>
+              </div>
+            </button>
+
+            <button className="flex flex-row h-16 gap-4 bg-white py-2 px-6 border-2 border-colBlue04 rounded-lg ">
+              <Image
+                src={AppStoreIcon}
+                alt="GooglePlayStoreIcon"
+                width={40}
+                onClick={() => setShowModal(true)}
+              />
+
+              <div className=" flex flex-col leading-tight ">
+                <p className="text-colBlue02 font-dmsans font-normal">
+                  Coming Soon{" "}
+                </p>
+                <h2 className="font-urbanist font-bold text-colBlue02 text-xl">
+                  Apple Store
+                </h2>
+              </div>
+            </button>
           </div>
         </div>
 
         {/* Left Container... Hero Image */}
 
-        <Image src={heroImage} alt="heroImage" width={600} height={600} />
+        <Image src={heroImage2} alt="heroImage" width={600} height={600} />
       </div>
 
       <RegistrationModal
