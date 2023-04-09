8612c6f74e054431dda3cb6ef531d1aab69a3342
MarsIfeanyi <marschalice@gmail.com>
2023-04-09 03:25:22 +0100

    Create Reusable Modal Component

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type Props = {
+  isVisible: boolean;
+  onClose: boolean | void | string | any;
+  children: React.ReactNode;
+};
+
+const Modal = ({ isVisible, onClose, children }: Props) => {
+  if (!isVisible) return null;
+
+  const handleClose = (e: any) => {
+    if (e.target.id === "wrapper") onClose();
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className="w-[600px] flex flex-col">
+        <button
+          className="text-white text-xl place-self-end"
+          onClick={() => onClose()}
+        >
+          X
+        </button>
+        <div className="bg-white p-2 rounded">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
