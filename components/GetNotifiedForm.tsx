376625f85a390ab6c896a27e5c4a886ffd188308
MarsIfeanyi <marschalice@gmail.com>
2023-04-09 20:35:15 +0100

    Create GetNotifiedForm

---
+++ components/GetNotifiedForm.tsx
@@ -0,0 +1,135 @@
+import { useForm } from "react-hook-form";
+
+useForm;
+
+type Props = {};
+
+const GetNotifiedForm = (props: Props) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitHandler = async (e: any) => {
+    // trigger() comes from useForm
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <div className="flex flex-col md:flex-row gap-4 px-2 py-10 ">
+        {/* Left */}
+        <div className="flex flex-col">
+          <div className="flex flex-col space-y-1 ">
+            <label
+              htmlFor="name"
+              className="text-colGray05 font-urbanist font-semibold  "
+            >
+              Name
+            </label>
+            <input
+              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              type="text"
+              //   placeholder="NAME"
+              {...register("name", {
+                required: true,
+                maxLength: 100,
+              })}
+            />
+            {errors.name && (
+              <p className="mt-1 text-primary-500">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col space-y-1 n mt-3">
+            <label
+              htmlFor="email"
+              className="text-colGray05 font-urbanist font-semibold  "
+            >
+              Email
+            </label>
+            <input
+              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              type="email"
+              //   placeholder="EMAIL"
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+            />
+            {errors.email && (
+              <p className="mt-1 text-primary-500">
+                {errors.email.type === "required" && "This field is required."}
+                {errors.email.type === "pattern" && "Invalid email address."}
+              </p>
+            )}
+          </div>
+        </div>
+
+        {/* Right */}
+
+        <div className="flex flex-col">
+          <div className="flex flex-col space-y-1 ">
+            <label
+              htmlFor="address"
+              className="text-colGray05 font-urbanist font-semibold  "
+            >
+              Address
+            </label>
+            <input
+              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              type="text"
+              //   placeholder="ADDRESS"
+              {...register("address", {
+                required: true,
+                maxLength: 100,
+              })}
+            />
+            {errors.name && (
+              <p className="mt-1 text-primary-500">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col space-y-1 mt-3 ">
+            <label
+              htmlFor="phoneNumber"
+              className="text-colGray05 font-urbanist font-semibold  "
+            >
+              Phone Number
+            </label>
+            <input
+              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              type="number"
+              //   placeholder="PHONE NUMBER"
+              {...register("phoneNumber", {
+                required: true,
+                maxLength: 100,
+              })}
+            />
+            {errors.name && (
+              <p className="mt-1 text-primary-500">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+          </div>
+
+          <button className="rounded bg-white mt-5 w-4/5 ml-10 py-2 text-colBlue04 font-urbanist font-semibold">
+            Get early access
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default GetNotifiedForm;
