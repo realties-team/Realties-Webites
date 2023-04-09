36dc5e3307bcad88cda000c09d4970bd107a1b70
MarsIfeanyi <marschalice@gmail.com>
2023-04-09 20:34:37 +0100

    Create get notified component

---
+++ components/GetNotified.tsx
@@ -0,0 +1,46 @@
+import { AppStoreIcon, PlayStoreIcon, rocket, threePhones } from "@/assets";
+import Image from "next/image";
+
+import GetNotifiedForm from "./GetNotifiedForm";
+
+type Props = {};
+
+const GetNotified = (props: Props) => {
+  return (
+    <>
+      {/* Background */}
+      <div
+        className="flex flex-row bg-colBlue04
+     mx-auto w-2/3 rounded-2xl mt-32"
+      >
+        {/* Main Container */}
+
+        <div className="flex flex-col md:flex-row  justify-between  gap-28">
+          {/* Left*/}
+          <div className="flex flex-col p-6 ml-10 mt-6">
+            <h2
+              className="font-urbanist font-bold text-white text-2xl
+                      
+            "
+            >
+              Get notified when <br /> we launch
+            </h2>
+            <Image
+              src={rocket}
+              alt="rocketLaunchImage"
+              width={100}
+              className="mt-6"
+            />
+          </div>
+
+          {/* Right */}
+
+          <GetNotifiedForm />
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default GetNotified;
