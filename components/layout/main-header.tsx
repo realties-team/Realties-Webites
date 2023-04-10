3095f5c78e3488fb981fbc07755717ae0e05e920
MarsIfeanyi <marschalice@gmail.com>
2023-04-10 14:34:50 +0100

    Fix responsiveness

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -4,6 +4,7 @@ import { AddUser, AddUser01, Search01, Logo, logo, Search } from "@/assets";
 import RegistrationModal from "../RegistrationModal";
 import EarlyAccessForm from "../EarlyAccessForm";
 import { useState } from "react";
+import HamburgerMenu from "./harmburger";
 
 //import AnchorLink from "react-anchor-link-smooth-scroll";
 
@@ -12,12 +13,18 @@ function MainHeader() {
 
   return (
     <header className="flex items-center justify-between mr-20 ">
-      <Image src={logo} alt="LogoImage" width={200} height={200} />
+      <Image
+        src={logo}
+        alt="LogoImage"
+        width={200}
+        height={200}
+        className="hidden md:flex"
+      />
 
       {/*Navigation Links  */}
 
       <nav>
-        <ul className="flex flex-row gap-8 text-xl font-urbanist font-medium">
+        <ul className=" flex-row gap-8 text-xl font-urbanist font-medium  hidden md:flex  ">
           <li className="text-colBlue01">
             <Link href="/">Home</Link>
           </li>
@@ -31,6 +38,31 @@ function MainHeader() {
             <Link href="/testimony">Testimony</Link>
           </li> */}
         </ul>
+
+        {/* Mobile Screen */}
+
+        <div className=" ">
+          <div className="flex flex-row md:hidden justify-between relative ">
+            <div className="flex flex-col ">
+              <Image src={logo} alt="LogoImage" width={150} height={150} />
+
+              <button
+                onClick={() => setShowModal(true)}
+                className="text-white font-dmsans font-  bg-colBlue02 px-4 py-2 rounded-lg  ml-10 "
+              >
+                Get early access
+              </button>
+            </div>
+
+            {/* <div className=" h-full w-[250px]   flex md:hidden justify-end  ">
+              <HamburgerMenu />
+            </div> */}
+
+            <div className=" fixed bottom-0 right-0 z-40 h-full w-[250px]  drop-shadow-xl flex md:hidden justify-end  ">
+              <HamburgerMenu />
+            </div>
+          </div>
+        </div>
       </nav>
 
       {/* ICONS */}
@@ -43,7 +75,7 @@ function MainHeader() {
 
       <button
         onClick={() => setShowModal(true)}
-        className="text-white font-dmsans font-medium bg-colBlue02 px-4 py-2 rounded-lg   "
+        className="text-white font-dmsans font-medium bg-colBlue02 px-4 py-2 rounded-lg hidden md:flex "
       >
         Get early access
       </button>
