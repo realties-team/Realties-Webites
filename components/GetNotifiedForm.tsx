55229a8e361e0eae1ddaa148be9de62daf608049
MarsIfeanyi <marschalice@gmail.com>
2023-04-10 16:34:09 +0100

    Fix Responsiveness

--- components/GetNotifiedForm.tsx
+++ components/GetNotifiedForm.tsx
@@ -20,10 +20,10 @@ const GetNotifiedForm = (props: Props) => {
   };
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className="flex flex-col md:flex-row gap-4 px-2 py-10 ">
+      <div className="flex flex-col md:flex-row gap-5 md:gap-10 px-2 py-10   ">
         {/* Left */}
-        <div className="flex flex-col">
-          <div className="flex flex-col space-y-1 ">
+        <div className="flex flex-col w-full ">
+          <div className="  flex flex-col space-y-1 ">
             <label
               htmlFor="name"
               className="text-colGray05 font-urbanist font-semibold  "
@@ -31,7 +31,7 @@ const GetNotifiedForm = (props: Props) => {
               Name
             </label>
             <input
-              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 text-white "
               type="text"
               //   placeholder="NAME"
               {...register("name", {
@@ -40,7 +40,7 @@ const GetNotifiedForm = (props: Props) => {
               })}
             />
             {errors.name && (
-              <p className="mt-1 text-primary-500">
+              <p className="mt-1 text-white">
                 {errors.name.type === "required" && "This field is required."}
                 {errors.name.type === "maxLength" && "Max length is 100 char."}
               </p>
@@ -55,7 +55,7 @@ const GetNotifiedForm = (props: Props) => {
               Email
             </label>
             <input
-              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              className=" text-white border border-white rounded-md outline-none bg-colBlue04 py-1 "
               type="email"
               //   placeholder="EMAIL"
               {...register("email", {
@@ -64,7 +64,7 @@ const GetNotifiedForm = (props: Props) => {
               })}
             />
             {errors.email && (
-              <p className="mt-1 text-primary-500">
+              <p className="mt-1 text-white">
                 {errors.email.type === "required" && "This field is required."}
                 {errors.email.type === "pattern" && "Invalid email address."}
               </p>
@@ -74,7 +74,7 @@ const GetNotifiedForm = (props: Props) => {
 
         {/* Right */}
 
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <div className="flex flex-col space-y-1 ">
             <label
               htmlFor="address"
@@ -83,7 +83,7 @@ const GetNotifiedForm = (props: Props) => {
               Address
             </label>
             <input
-              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              className=" text-white border border-white rounded-md outline-none bg-colBlue04 py-1 "
               type="text"
               //   placeholder="ADDRESS"
               {...register("address", {
@@ -92,7 +92,7 @@ const GetNotifiedForm = (props: Props) => {
               })}
             />
             {errors.name && (
-              <p className="mt-1 text-primary-500">
+              <p className="mt-1 text-white">
                 {errors.name.type === "required" && "This field is required."}
                 {errors.name.type === "maxLength" && "Max length is 100 char."}
               </p>
@@ -107,7 +107,7 @@ const GetNotifiedForm = (props: Props) => {
               Phone Number
             </label>
             <input
-              className=" border border-white rounded-md outline-none bg-colBlue04 py-1 "
+              className="text-white border border-white rounded-md outline-none bg-colBlue04 py-1 "
               type="number"
               //   placeholder="PHONE NUMBER"
               {...register("phoneNumber", {
@@ -116,14 +116,14 @@ const GetNotifiedForm = (props: Props) => {
               })}
             />
             {errors.name && (
-              <p className="mt-1 text-primary-500">
+              <p className="mt-1 text-white">
                 {errors.name.type === "required" && "This field is required."}
                 {errors.name.type === "maxLength" && "Max length is 100 char."}
               </p>
             )}
           </div>
 
-          <button className="rounded bg-white mt-5 w-4/5 ml-10 py-2 text-colBlue04 font-urbanist font-semibold">
+          <button className="rounded bg-white mt-10 md:mt-5 w-full md:w-4/5  md:ml-10 py-2 text-colBlue04 font-urbanist font-semibold">
             Get early access
           </button>
         </div>
