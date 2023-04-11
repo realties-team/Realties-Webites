55147b0067244c5585e61bac4610b8ecf04c94b4
MarsIfeanyi <marschalice@gmail.com>
2023-04-11 23:46:57 +0100

    Move component to home folder

---
+++ components/home/RegistrationModal.tsx
@@ -0,0 +1,37 @@
+import { Xmark } from "@/assets";
+import React from "react";
+
+type Props = {
+  isVisible: boolean;
+  onClose: boolean | void | string | any;
+  children: React.ReactNode;
+};
+
+const RegistrationModal = ({ isVisible, onClose, children }: Props) => {
+  if (!isVisible) return null;
+
+  const handleClose = (e: any) => {
+    if (e.target.id === "wrapper") onClose();
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-none flex justify-center items-center"
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className=" mt-32 md:mt-0 w-[430px] md:w-[700px] flex flex-col relative">
+        <button
+          className="text-black text-2xl absolute top-6 right-10   "
+          onClick={() => onClose()}
+        >
+          X
+        </button>
+
+        <div className="bg-white p-2 rounded-2xl">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationModal;
