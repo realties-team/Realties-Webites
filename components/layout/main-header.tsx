ccc3ebe4aa517029091e31f53330551bf2f3f913
MarsIfeanyi <marschalice@gmail.com>
2023-04-11 23:44:20 +0100

    Update file

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import { AddUser, AddUser01, Search01, Logo, logo, Search } from "@/assets";
-import RegistrationModal from "../RegistrationModal";
-import EarlyAccessForm from "../EarlyAccessForm";
+import RegistrationModal from "../home/RegistrationModal";
+import EarlyAccessForm from "../home/EarlyAccessForm";
 import { useState } from "react";
 import HamburgerMenu from "./harmburger";
-
 //import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function MainHeader() {
   const [showModal, setShowModal] = useState<boolean>(false);
 
   return (
-    <header className="flex items-center justify-between mr-20 ">
+    <header className="flex items-center justify-between mx-12 md:mx-32 ">
       <Image
         src={logo}
         alt="LogoImage"
         width={200}
         height={200}
-        className="hidden md:flex"
+        className="hidden md:flex -ml-20"
       />
 
       {/*Navigation Links  */}
@@ -34,12 +33,7 @@ function MainHeader() {
           <li className="text-colGray02">
             <Link href="/featured">Featured</Link>
           </li>
-          {/* <li className="text-colGray02">
-            <Link href="/testimony">Testimony</Link>
-          </li> */}
         </ul>
-
-        {/* Mobile Screen */}
 
         <div className=" ">
           <div className="flex flex-row md:hidden justify-between relative ">
@@ -48,15 +42,11 @@ function MainHeader() {
 
               <button
                 onClick={() => setShowModal(true)}
-                className="text-white font-dmsans font-  bg-colBlue02 px-4 py-2 rounded-lg  ml-10 "
+                className="text-white font-dmsans mr-20 flex   bg-colBlue02 px-4 py-2 rounded-lg  ml-10 "
               >
                 Get early access
               </button>
             </div>
-
-            {/* <div className=" h-full w-[250px]   flex md:hidden justify-end  ">
-              <HamburgerMenu />
-            </div> */}
 
             <div className=" fixed bottom-0 right-0 z-40 h-full w-[250px]  drop-shadow-xl flex md:hidden justify-end  ">
               <HamburgerMenu />
@@ -64,14 +54,6 @@ function MainHeader() {
           </div>
         </div>
       </nav>
-
-      {/* ICONS */}
-
-      {/* <div className="gap-6 flex flex-row text-colBlue01 ">
-        <Image src={Search01} alt="searchIcon" width={25}></Image>
-
-        <Image src={AddUser01} alt="addUserIcon" width={25}></Image>
-      </div> */}
 
       <button
         onClick={() => setShowModal(true)}
